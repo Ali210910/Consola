e11286da258a9d4a5f2f6c92ab6237f8b1ebc7ea
Ali210910 <110068347+Ali210910@users.noreply.github.com>
2022-09-18 16:50:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Mi proyecto en la consola");
+var nombre="Zaid Ali";
+var apellido="Treviño Nuñez";
+var completo=nombre.concat(apellido);
+console.log(completo);
+var a=24;
+var b=26;
+var suma=a+b;
+console.log(suma);
+var resta=b-a;
+console.log(resta);
+var multiplicación=a*b;
+console.log(multiplicación);
+var división=b/a;
+console.log(división);
+
